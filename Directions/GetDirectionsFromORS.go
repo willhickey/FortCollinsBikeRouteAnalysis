bbc25384be822fbe75e04cb7c976c611abc8d66d
willhickey <csu_hickey@Yahoo.com>
2018-02-11 21:26:09 -0700

    go fmt

--- Directions/GetDirectionsFromORS.go
+++ Directions/GetDirectionsFromORS.go
@@ -7,8 +7,8 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
-	"net/url"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"time"
@@ -16,8 +16,8 @@ import (
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/tidwall/gjson"
 )
-	
-func main(){
+
+func main() {
 	orsKey, _ := ioutil.ReadFile("openrouteservicekey.txt")
 	baseurl := "https://api.openrouteservice.org/directions?"
 	params := url.Values{}
@@ -42,18 +42,17 @@ func main(){
 	params.Add("optimized", "true")
 	params.Add("options", "{}")
 
-
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter password: ")
 	pw, _ := reader.ReadString('\n')
-	
+
 	//TODO: get Trusted Connection working to eliminate password
-	
+
 	//connectString := "sqlserver://scriptuser:"+ strings.TrimSpace(pw) + "@localhost:12345?database=FortCollinsBikeRouteAnalysis&encrypt=disable"
 	//dbConnection, errdb := sql.Open("mssql", connectString)
 	//dbConnection, errdb := sql.Open("mssql", "Server=W520\\SQLEXPRESS;user id=;encrypt=disable;database=FortCollinsBikeRouteAnalysis")
 	//dbConnection, errdb := sql.Open("mssql", "Server=127.0.0.1;port=12345;user id=;password=;database=FortCollinsBikeRouteAnalysis")
-	dbConnection, errdb := sql.Open("mssql", "Server=localhost\\SQLEXPRESS;user id=scriptuser;password="+ strings.TrimSpace(pw) + ";database=FortCollinsBikeRouteAnalysis")		//works with Browser running
+	dbConnection, errdb := sql.Open("mssql", "Server=localhost\\SQLEXPRESS;user id=scriptuser;password="+strings.TrimSpace(pw)+";database=FortCollinsBikeRouteAnalysis") //works with Browser running
 	if errdb != nil {
 		fmt.Println(" Error open db:", errdb.Error())
 	}
@@ -64,21 +63,21 @@ func main(){
 			WHERE Source = 'ors' AND RawDirections IS NULL`
 
 	rows, err := dbConnection.Query(getRouteSQL)
-	if err!= nil {
+	if err != nil {
 		fmt.Println("Error querying db:", err.Error())
 	}
-	for rows.Next(){
+	for rows.Next() {
 		var (
-				id int
-				originLat string
-				originLon string
-				destLat string
-				destLon string
-			)
-		
+			id        int
+			originLat string
+			originLon string
+			destLat   string
+			destLon   string
+		)
+
 		rows.Scan(&id, &originLat, &originLon, &destLat, &destLon)
 		coords := url.Values{}
-		coords.Add("coordinates", originLon + "," + originLat + "|" + destLon + "," + destLat)
+		coords.Add("coordinates", originLon+","+originLat+"|"+destLon+","+destLat)
 		url := baseurl + params.Encode() + "&" + coords.Encode()
 		//fmt.Println(url)
 		//fmt.Println()
@@ -86,30 +85,30 @@ func main(){
 		if err != nil {
 			fmt.Println("Error: " + err.Error())
 		}
-		
+
 		body, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			fmt.Println("Error: " + err.Error())
 		}
 		response.Body.Close()
-		
+
 		value := gjson.Get(string(body), "routes.0.geometry")
 		distance := gjson.Get(string(body), "routes.0.summary.distance")
-		
+
 		//build the route polyline in lat,lon format. ORS returns lon,lat but I'm consuming this data in leaflet so transposing before writing to db.
-		routePolyline := "[";
+		routePolyline := "["
 		for _, point := range value.Array() {
 			routePolyline = routePolyline + "[" + point.Array()[1].String() + "," + point.Array()[0].String() + "],"
 		}
-		routePolyline = routePolyline + "]";
-		
+		routePolyline = routePolyline + "]"
+
 		fmt.Println(id)
 		updateSQL := fmt.Sprintf("UPDATE dbo.route SET RawDirections = '%v', BikeDistance = %v, OverviewPolyline = '%v' WHERE ID = %v", string(body), distance, routePolyline, id)
 		//fmt.Println(updateSQL)
 		dbConnection.Exec(updateSQL)
-		
+
 		//ORS free account only allows 40 requests per minute, so sleep a bit
-		time.Sleep(2000000000);		//2 seconds
+		time.Sleep(2000000000) //2 seconds
 	}
-	
+
 }
